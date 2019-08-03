68bb08b731f67b5d117762336a903a5cfece6d9a
Paul Raab <pvraab@hotmail.com>
2019-08-03 13:47:01 -0600

    Initial APIs and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const userRoutes = require("./users");
+const itemRoutes = require("./items");
+const orderRoutes = require("./orders");
 const bookRoutes = require("./books");
 
-// Book routes
+// Routes
+router.use("/users", userRoutes);
+router.use("/items", itemRoutes);
+router.use("/orders", orderRoutes);
 router.use("/books", bookRoutes);
 
 module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const usersController = require("../../controllers/usersController");
+
+// Matches with "/api/users"
+router.route("/")
+  .get(usersController.findAll);
+
+module.exports = router;
